32bc092544646258500456ece757caf975d4d9b0
Julian <thejulianjuko@gmail.com>
2023-12-02 19:01:54 +0100

    solved day 2 puzzle 2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -30,12 +30,35 @@ public class Day2 {
         System.out.println(sum);
     }
 
-    private void puzzle2(){
+    private void puzzle2(String[] lines){
+        int sum = 0;
+        for(int i = 0; i < 100; i++){
+            Map<String, Integer> counts = new HashMap<>();
 
+            String[] sets = lines[i].split(": ")[1].split("; ");
+
+            for(var set : sets){
+                String[] pulls = set.split(", ");
+                for (var pull : pulls){
+                    int num = Integer.parseInt(pull.split(" ")[0]);
+                    String color = pull.split(" ")[1].trim();
+
+                    if(!counts.containsKey(color) || counts.get(color) < num){
+                        counts.put(color, num);
+                    }
+                }
+            }
+            int power = 1;
+            for(int num : counts.values()){
+                power *= num;
+            }
+            sum += power;
+        }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
         Day2 day2 = new Day2();
-        day2.puzzle1(InputReader.getLines(2));
+        day2.puzzle2(InputReader.getLines(2));
     }
 }
