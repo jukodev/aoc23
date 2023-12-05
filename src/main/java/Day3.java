3d048147f8f03cd4faea526084a81c08a77b860b
Julian <thejulianjuko@gmail.com>
2023-12-05 21:41:57 +0100

    solved day 3 puzzle2 (skullemoji)

--- src/main/java/Day3.java
+++ src/main/java/Day3.java
@@ -49,26 +49,50 @@ public class Day3 {
                 char currentChar = lines[i].charAt(j);
                 if(currentChar == '*'){
                     int numberCount = 0;
+                    StringBuilder[] numbers = new StringBuilder[2];
+                    numbers[0] = new StringBuilder();
+                    numbers[1] = new StringBuilder();
                     boolean currentStreak = false;
                     for (int k = -1; k <= 1; k++) {
+                        currentStreak = false;
                         for (int l = -1; l <= 1; l++) {
+
                             if(Character.isDigit(lines[i+k].charAt(j+l))){
+                                if(currentStreak){
+                                    continue;
+                                }
                                 currentStreak = true;
+                                numberCount++;
+                                if(numberCount > 2)break;
+                                int offSet = 1;
+                                while (lines[i+k].length() > j+l+offSet && Character.isDigit(lines[i+k].charAt(j+l+offSet))){
+                                    numbers[numberCount-1].append(lines[i+k].charAt(j+l+offSet));
+                                    offSet++;
+                                }
+                                offSet = -0;
+                                while (j+l+offSet >= 0 && Character.isDigit(lines[i+k].charAt(j+l+offSet))){
+                                    numbers[numberCount-1].insert(0, lines[i+k].charAt(j+l+offSet));
+                                    offSet--;
+                                }
 
                             }else{
                                 currentStreak = false;
                             }
                         }
                     }
+                    if(numberCount != 2)continue;
+                    sum += Integer.parseInt(numbers[0].toString()) * Integer.parseInt(numbers[1].toString());
                 }
 
             }
         }
+
+        System.out.println(sum);
     }
 
 
     public static void main(String[] args) {
         Day3 day3 = new Day3();
-        day3.puzzle1(InputReader.getLines(3));
+        day3.puzzle2(InputReader.getLines(3));
     }
 }
