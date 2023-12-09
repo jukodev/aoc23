faa26c230e771bf3d2e930bc14fe3f5bdf98fd6f
Julian <thejulianjuko@gmail.com>
2023-12-09 23:37:43 +0100

    fix

--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -21,10 +21,7 @@ public class Day9 {
     }
 
     private long getLastElement(int[] nums){
-        int[] diffs = new int[nums.length -1];
-        for (int i = 1; i < nums.length; i++) {
-            diffs[i-1] = nums[i] - nums[i-1];
-        }
+        int[] diffs = getDiff(nums);
         if(Arrays.stream(diffs).allMatch(d -> d == 0)){
             return nums[0];
         }
